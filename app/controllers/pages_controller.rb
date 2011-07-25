1531ee02483e08769abc5a2afbe5382bbf7a826b
Adam Maitland <adam@adammaitland.com>
2011-07-25 08:21:20 -0700

    start again at 10

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,11 @@
 class PagesController < ApplicationController
   def home
     @title = "Home"
+    if signed_in?
+      @micropost = Micropost.new
+      @feed_items = current_user.feed.paginate(:page => params[:page],
+                                               :per_page => 10)
+    end
   end
 
   def contact
